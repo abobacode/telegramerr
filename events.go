d7f232652ca4d890074cde8ed2ef885c0b79c2f3
abobacode <ik451080@gmail.com>
2024-07-31 15:43:12 +0300

    fix telegramerr telegram bot

--- events.go
+++ events.go
@@ -3,12 +3,19 @@ package telegramerr
 import (
 	"fmt"
 	"log"
+	time2 "time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func (b *Bot) SendErrorMessage(err error) {
-	msg := tgbotapi.NewMessage(b.chatID, fmt.Sprintf("🔴 ERROR: %s", err))
+const ERROR = "🔴 ERROR: %s" +
+	"\n" + "⚙️ SERVICE: %s" +
+	"\n" + "🕘 TIME: %s" +
+	"\n" + "⛓️ TRACING: //TODO"
+
+func (b *Bot) SendErrorMessage(err error, service string) {
+	time := time2.Now()
+	msg := tgbotapi.NewMessage(b.chatID, fmt.Sprintf(ERROR, err, service, time))
 	if _, err := b.api.Send(msg); err != nil {
 		log.Printf("Failed to send error message: %v", err)
 	}
